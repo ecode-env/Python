0e5965d2a1e169d2b70ab44e50a171db8000203b
eyobScript <eyobbmulugeta@gmail.com>
2024-12-11 09:23:17 +0300

    day 3

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,5 @@
+numberToCheck = int(input("What is the number you want to check? "))
+if numberToCheck % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,21 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+bills = 0
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bills+=5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bills +=7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bills +=12
+        print("Youth tickets are $12.")
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "y":
+        bills+=3
+    print(f"Your bills are {bills}$.")
 else:
     print("Sorry you have to grow taller before you can ride.")
