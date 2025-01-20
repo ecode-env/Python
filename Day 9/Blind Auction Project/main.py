d7478522f54d5d1fc1d029b57d4431e7b5669051
eyobScript <eyobbmulugeta@gmail.com>
2025-01-20 10:37:30 +0300

    Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,27 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+from art import logo
+print(logo)
+
+bidders = {}
+winner = None
+
+other = True
+
+while other:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bidders[name] = bid
+    yes_or_no = input("Are there any other bidders: Type 'yes' or 'no'")
+    print("\n" * 100)
+    if yes_or_no == 'no':
+        other = False
+
+
+for key in bidders:
+        if bidders[key] > 0:
+            winner = key
+
+print(f"The winner is {winner}. with a bid of ${bidders[winner]}")
 
