d6de32947fd84af00c9980b30bf593ae7f680b76
eyobScript <eyobbmulugeta@gmail.com>
2025-01-23 14:40:27 +0300

    Day 16 OOP

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,13 +1,23 @@
-from turtle import Turtle, Screen
-my_turtle = Turtle()
-print(my_turtle)
+# from turtle import Turtle, Screen
+# my_turtle = Turtle()
+# print(my_turtle)
+#
+# my_turtle.shape("turtle")
+# my_turtle.color("coral")
+# my_turtle.forward(100)
+#
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_turtle.shape("turtle")
-my_turtle.color("coral")
-my_turtle.forward(100)
+from prettytable import PrettyTable
+table = PrettyTable()
 
+table.add_column("Pokemon Name", [ "Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
+table.align = 'l'
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+print(table)
