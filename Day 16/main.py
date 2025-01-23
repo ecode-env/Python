ce923e91a0c36b9e4789bd4bc41b3fcdf2720a8b
eyobScript <eyobbmulugeta@gmail.com>
2025-01-23 10:36:33 +0300

    Day 16 OOP

---
+++ Day 16/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+my_turtle = Turtle()
+print(my_turtle)
+
+my_turtle.shape("turtle")
+my_turtle.color("coral")
+my_turtle.forward(100)
+
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
