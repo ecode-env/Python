01a9aa5f2a09b8862e8989ea5a59e041393a0bbd
eyobScript <eyobbmulugeta@gmail.com>
2025-01-27 11:53:44 +0300

    day 18 wed write the code which is dash line in turtle

---
+++ Day 18/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+
+eyob_the_turtle = Turtle()
+# eyob_the_turtle.shape("turtle")
+# eyob_the_turtle.color('red')
+
+for _ in range(15):
+    eyob_the_turtle.forward(10)
+    eyob_the_turtle.pendown()
+    eyob_the_turtle.forward(10)
+    eyob_the_turtle.penup()
+
+
+# eyob_the_turtle.right(90)
+# eyob_the_turtle.forward(100)
+# eyob_the_turtle.right(90)
+# eyob_the_turtle.forward(100)
+# eyob_the_turtle.right(90)
+# eyob_the_turtle.forward(100)
+# eyob_the_turtle.right(90)
+# eyob_the_turtle.forward(100)
+
+
+
+
+
+
+
+screen = Screen()
+
+screen.exitonclick()
