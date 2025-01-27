63e94c163889bd13b3bb66ab151f925374e12070
eyobScript <eyobbmulugeta@gmail.com>
2025-01-27 14:47:21 +0300

    day 18 'Turtle Challenge 3 - Drawing Different Shapes'

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle, Screen
 
 
@@ -38,8 +39,26 @@ eyob_the_turtle = Turtle()
 #     angel = angel + 1
 #     print(angel)
 
+# Array of colors for Python turtle
+turtle_colors = [
+    "red", "blue", "green", "yellow", "purple", "orange",
+    "pink", "brown", "black", "white", "gray", "cyan",
+    "magenta", "lime", "turquoise", "gold", "navy", "coral",
+    "indigo", "violet", "salmon", "chocolate", "beige", "dark green"
+]
 
 
+
+
+def draw_shape(num_sides):
+    angele = 360 / num_sides
+    for _ in range(num_sides):
+        eyob_the_turtle.forward(100)
+        eyob_the_turtle.right(angele)
+
+for shape_side_n in range(3, 11):
+    eyob_the_turtle.color(random.choice(turtle_colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 
