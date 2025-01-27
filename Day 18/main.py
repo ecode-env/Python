b576ce47ce272665409c16444c619568df1d9ece
eyobScript <eyobbmulugeta@gmail.com>
2025-01-27 14:22:18 +0300

    day 18 ''write the code which is gives challenge from angela ''

--- Day 18/main.py
+++ Day 18/main.py
@@ -5,11 +5,14 @@ eyob_the_turtle = Turtle()
 # eyob_the_turtle.shape("turtle")
 # eyob_the_turtle.color('red')
 
-for _ in range(15):
-    eyob_the_turtle.forward(10)
-    eyob_the_turtle.pendown()
-    eyob_the_turtle.forward(10)
-    eyob_the_turtle.penup()
+
+# dash line
+
+# for _ in range(15):
+#     eyob_the_turtle.forward(10)
+#     eyob_the_turtle.pendown()
+#     eyob_the_turtle.forward(10)
+#     eyob_the_turtle.penup()
 
 
 # eyob_the_turtle.right(90)
@@ -21,8 +24,19 @@ for _ in range(15):
 # eyob_the_turtle.right(90)
 # eyob_the_turtle.forward(100)
 
+# eyob_the_turtle.forward(80)
+# eyob_the_turtle.right(72)
 
-
+# angel = 3
+# rotate = True
+# while rotate:
+#     if 360 / angel == 0:
+#         rotate = False
+#     for _ in range(angel):
+#         eyob_the_turtle.right(360 / angel)
+#         eyob_the_turtle.forward(100)
+#     angel = angel + 1
+#     print(angel)
 
 
 
