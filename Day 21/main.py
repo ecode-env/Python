0697bb38a544a62f3f8c7835d21a80ad0a46dfb0
eyobScript <eyobbmulugeta@gmail.com>
2025-01-29 12:22:07 +0300

    day 21 "We have done the count scoreboard function"

--- Day 21/main.py
+++ Day 21/main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from snake import Snake
 from food import Food
-
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -13,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -30,6 +31,7 @@ while game_is_on:
     snake.move()
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 
--- Day 21/snake.py
+++ Day 21/snake.py
@@ -1,13 +1,11 @@
 from turtle import Turtle
 
-
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
-
 
 class Snake:
     def __init__(self):
@@ -23,15 +21,22 @@ class Snake:
             segment.goto(position)
             self.segments.append(segment)
 
-
     def up(self):
-        self.head.heading() != DOWN and self.head.setheading(UP)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
     def down(self):
-        self.head.heading() != UP and self.head.setheading(DOWN)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
     def left(self):
-        self.head.heading() != RIGHT and self.head.setheading(LEFT)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
     def right(self):
-        self.head.heading() != LEFT and self.head.setheading(RIGHT)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
