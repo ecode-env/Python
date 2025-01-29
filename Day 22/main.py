ab88718da00a016ae7e679e1bdfe852879f3877c
eyobScript <eyobbmulugeta@gmail.com>
2025-01-29 18:36:43 +0300

    day 22 "we have done paddle game."

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,4 +1,8 @@
-from turtle import Turtle, Screen
+import time
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -6,34 +10,43 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.penup()
-paddle.shapesize(5, 1)
-paddle.goto(350, 0)
-
-def go_up():
-    paddle.goto(paddle.xcor(), paddle.ycor() + 20)
-def go_down():
-    paddle.goto(paddle.xcor(), paddle.ycor() - 20)
+score = Scoreboard()
+ball = Ball()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
 
 is_game_on = True
 while is_game_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
 
+    if ball.xcor() > 380:
+        score.l_point()
+        ball.reset_position()
 
+    if ball.xcor() < -380:
+        score.r_point()
+        ball.reset_position()
 
+    if score.l_score == 5:
+        is_game_on = False
+        score.game_over()
 
-
-
-
+    if score.r_score == 5:
+        is_game_on = False
+        score.game_over()
 
 screen.exitonclick()
