a664378263cc32755d8b95113bb29e3ddbfbf3fd
eyobScript <eyobbmulugeta@gmail.com>
2025-01-30 15:13:22 +0300

    day 24 "we have done read and write a file which is save our score."

--- Day 24/002 Snake-Project-Code-from-Day-21/scoreboard.py
+++ Day 24/002 Snake-Project-Code-from-Day-21/scoreboard.py
@@ -1,3 +1,4 @@
+from time import sleep
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
@@ -8,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', mode='r') as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 250)
@@ -21,7 +23,10 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.high_score < self.score:
-            self.high_score = self.score
+            with open('data.txt', mode='w') as file:
+                self.high_score = self.score
+                with open('data.txt', mode='w') as file:
+                    file.write(f'{self.score}')
         self.score = 0
         self.update_scoreboard()
 
