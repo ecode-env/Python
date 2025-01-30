c96f2ebe2a2b524933344d320a4676b1fa377ffb
eyobScript <eyobbmulugeta@gmail.com>
2025-01-30 12:43:57 +0300

    day 24 "We added a function which is restart the game."

--- Day 24/002 Snake-Project-Code-from-Day-21/snake.py
+++ Day 24/002 Snake-Project-Code-from-Day-21/snake.py
@@ -13,6 +13,12 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
