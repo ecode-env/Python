baa1d3a78a7bd928932ee547fe561aac247863a1
eyobScript <eyobbmulugeta@gmail.com>
2025-01-30 14:34:01 +0300

    day 24 "first task to create read and write a file."

---
+++ Day 24/main.py
@@ -0,0 +1,14 @@
+# for read file from current folder
+# with open('my_text.txt') as file:
+#     content = file.read()
+#     print(content)
+from importlib.resources import contents
+
+# with open('my_text.txt', mode='w') as file:
+#     content = file.write('Its delete all text and add this text')
+
+# with open('my_text.txt', mode='a') as file:
+#     file.write('\nIts add a file in the text without over write.')
+
+# with open('new_text_file.txt', mode='w') as file:
+#     file.write('hello, this is new file.')
