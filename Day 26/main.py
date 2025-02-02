27c0b133555ce8d66f4d01804afbd8f505477995
eyobScript <eyobbmulugeta@gmail.com>
2025-02-02 12:26:07 +0300

    task 3 day 26

--- Day 26/main.py
+++ Day 26/main.py
@@ -3,20 +3,42 @@
 
 ## second task
 
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-
-# new_name_list = [name for name in names if len(name) < 5]
+## names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 #
-# print(new_name_list)
+# # new_name_list = [name for name in names if len(name) < 5]
+# #
+# # print(new_name_list)
+#
+# #capital_name = [name.upper() for name in names if len(name) > 5]
+#
+# #print(capital_name)
+# import random
+#
+# student_score = { student: random.randint(1,100) for student in names }
+#
+# passed_student = {name: score for (name, score) in student_score.items() if score > 60}
+#
+# print(passed_student)
 
-#capital_name = [name.upper() for name in names if len(name) > 5]
 
-#print(capital_name)
-import random
+import pandas
 
-student_score = { student: random.randint(1,100) for student in names }
+student_dict = {
+    "student": ["eyob","angela"],
+    "score": [80,90]
+}
+student_df = pandas.DataFrame(student_dict)
 
-passed_student = {name: score for (name, score) in student_score.items() if score > 60}
 
-print(passed_student)
+## Loop through data frame
+# for (key, value) in student_df.items():
+#     print(value)
 
+## Loop through row of data frame
+
+for (index, row) in student_df.iterrows():
+    #print(row.student)
+    #print(row)
+
+    if row.student == 'eyob':
+        print(row.score)
