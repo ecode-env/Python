4edcc2fb6f92680af6c7dd38560bde8316d14477
eyobScript <eyobbmulugeta@gmail.com>
2025-02-02 12:07:01 +0300

    task 2 day 26

--- Day 26/main.py
+++ Day 26/main.py
@@ -9,6 +9,14 @@ names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 #
 # print(new_name_list)
 
-capital_name = [name.upper() for name in names if len(name) > 5]
+#capital_name = [name.upper() for name in names if len(name) > 5]
 
-print(capital_name)
+#print(capital_name)
+import random
+
+student_score = { student: random.randint(1,100) for student in names }
+
+passed_student = {name: score for (name, score) in student_score.items() if score > 60}
+
+print(passed_student)
+
