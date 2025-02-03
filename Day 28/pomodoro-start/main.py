04363ec1259e43694c4e3e47c0a65f62ab089a18
eyobScript <eyobbmulugeta@gmail.com>
2025-02-03 19:12:32 +0300

    We have added into our Pomodor day 28

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -9,12 +9,29 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(1 * 60)
+    global reps
+    reps +=1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 60:
+        titel_label.config(text='Break', fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        titel_label.config(text='Break', fg=PINK)
+        count_down(short_break_sec)
+    else:
+        titel_label.config(text='Work', fg=GREEN)
+        count_down(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -27,6 +44,12 @@ def count_down(count):
 
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ''
+        for _ in range(math.floor(reps/2)):
+            mark +='✔'
+        label_checkmark.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -44,7 +67,7 @@ canvas.grid(column=2, row=2)
 ## Label
 
 titel_label = Label(text='Timer', font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
-label_checkmark = Label(text='✔', font=(FONT_NAME, 35, 'italic'), bg=YELLOW)
+label_checkmark = Label(font=(FONT_NAME, 35, 'italic'), bg=YELLOW)
 
 
 titel_label.grid(column=2, row=0)
