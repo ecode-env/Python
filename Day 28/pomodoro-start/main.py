24d49f88a27409709c47c8aa621c21ba7e3dd4e6
eyobScript <eyobbmulugeta@gmail.com>
2025-02-03 18:48:16 +0300

    We have added into our Pomodor day 28

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,9 +12,21 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(1 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f'0{count_sec}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -24,11 +37,26 @@ window.config(padx=100,pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224,  bg=YELLOW, highlightthickness=0)
 time_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=time_image)
-canvas.create_text(100,140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+timer_text = canvas.create_text(100,140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=2, row=2)
+
 
 ## Label
 
-label = Label(text='Timer', font=(FONT_NAME, 35, 'italic'))
+titel_label = Label(text='Timer', font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
+label_checkmark = Label(text='✔', font=(FONT_NAME, 35, 'italic'), bg=YELLOW)
+
+
+titel_label.grid(column=2, row=0)
+label_checkmark.grid(column=2, row=4)
+
+## Button
+
+start_btn = Button(text='Start', highlightthickness=0, command=start_timer)
+reset_btn = Button(text='Reset', highlightthickness=0)
+
+start_btn.grid(column=1, row=3)
+reset_btn.grid(column=3, row=3)
+
 
 window.mainloop()
