bb85ecde7686aa24c0ffc448510bd8c9c6692771
eyobScript <eyobbmulugeta@gmail.com>
2025-02-04 16:45:19 +0300

    Catch file not found error and functionality of raise  'day 30'

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,22 +1,30 @@
 ## File not found
+# 
+# try:
+#     # Try to open the file and access the dictionary key
+#     file = open('a_text.txt')
+#     dictio = {'key': 'value'}
+#     #print(dictio['ello'])  # This will raise a KeyError
+# except FileNotFoundError:
+#     # Handle file not found error
+#     file = open('a_text.txt', 'w')
+#     file.write('Hello World')
+# except KeyError as error:
+#     # Handle KeyError for the dictionary lookup
+#     print(f'The key {error} does not exist.')
+# else:
+#     # If no exceptions occur, read the file content
+#     content = file.read()
+#     print(content)
+# finally:
+#     # Always close the file
+#     file.close()
+#     print('File was closed.')
 
-try:
-    # Try to open the file and access the dictionary key
-    file = open('a_text.txt')
-    dictio = {'key': 'value'}
-    print(dictio['ello'])  # This will raise a KeyError
-except FileNotFoundError:
-    # Handle file not found error
-    file = open('a_text.txt', 'w')
-    file.write('Hello World')
-except KeyError as error:
-    # Handle KeyError for the dictionary lookup
-    print(f'The key "{error}" does not exist.')
+height = float(input('enter you height: '))
+weight = int(input('enter you weight: '))
+
+if height > 3:
+    raise TypeError('You have to under 3m ')
 else:
-    # If no exceptions occur, read the file content
-    content = file.read()
-    print(content)
-finally:
-    # Always close the file
-    file.close()
-    print('File was closed.')
+    print(weight /  height** 2)
