1a612d12b8cb54813f57fa8d38dcf226e051eeb6
eyobScript <eyobbmulugeta@gmail.com>
2025-02-04 16:03:48 +0300

    Catch file not found error 'day 30'

---
+++ Day 30/main.py
@@ -0,0 +1,22 @@
+## File not found
+
+try:
+    # Try to open the file and access the dictionary key
+    file = open('a_text.txt')
+    dictio = {'key': 'value'}
+    print(dictio['ello'])  # This will raise a KeyError
+except FileNotFoundError:
+    # Handle file not found error
+    file = open('a_text.txt', 'w')
+    file.write('Hello World')
+except KeyError as error:
+    # Handle KeyError for the dictionary lookup
+    print(f'The key "{error}" does not exist.')
+else:
+    # If no exceptions occur, read the file content
+    content = file.read()
+    print(content)
+finally:
+    # Always close the file
+    file.close()
+    print('File was closed.')
