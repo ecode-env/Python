59944eb1bc69d938508e196c39e550b2e5d1851a
eyobScript <eyobbmulugeta@gmail.com>
2025-02-04 11:52:21 +0300

    We did the first task which is created by canvas 'day 29'

--- Day 29/password-manager-start/main.py
+++ Day 29/password-manager-start/main.py
@@ -7,7 +7,7 @@ from tkinter import *
 
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=70, pady=70)
 
 # Canvas for Logo
 canvas = Canvas(width=200, height=200)
@@ -15,40 +15,33 @@ logo_image = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_image)
 canvas.grid(column=1, row=0)
 
-# Entry
-website = Entry(width=35)
-website.grid(column=1, row=1, columnspan=2)
+# Entry Fields
+website = Entry(width=43)
+website.focus()
+website.grid(row=1, column=1, columnspan=2)
 
-email = Entry(width=35)
-email.grid(column=1, row=2, columnspan=2)
+email = Entry(width=43)
+email.insert(0, 'eyob@gmail.com')
+email.grid(row=2, column=1, columnspan=2)
 
-password = Entry(width=21)
-password.grid(column=1, row=3)
+password = Entry(width=22)  # Adjust width so it fits with the button
+password.grid(row=3, column=1, sticky="w")
 
-## Label
-
+# Labels
 website_label = Label(text='Website:')
 website_label.grid(column=0, row=1)
 
-
 email_label = Label(text='Email/Username:')
-email_label .grid(column=0, row=2)
-
+email_label.grid(column=0, row=2)
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
 
+# Buttons
+generate_password = Button(text='Generate Password', width=15)
+generate_password.grid(row=3, column=2)
 
-## Button
-
-generate_password = Button(text='Generate Password', width=12)
-generate_password.grid(column=2, row=3)
-
-add = Button(text='Add', width=36)
-add.grid(column=1,row=4, columnspan=2)
-
-
-
-
+add = Button(text='Add', width=40)
+add.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
