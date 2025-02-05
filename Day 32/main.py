a1a1ce71094386a0ceafc18e4b0c2c8f72b9eea7
eyobScript <eyobbmulugeta@gmail.com>
2025-02-05 18:52:15 +0300

    birthday wisher 'day 32'

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,3 +1,40 @@
+import datetime
+import random
+import smtplib
+
+now = datetime.datetime.now()
+weekday = now.weekday()
+
+my_gmail = 'eyobbmulugeta@gmail.com'
+password = 'ftgpfxwyebrazxdf'
+
+if weekday == 0:
+    with open('quotes.txt') as quotes:
+        quote = random.choice(quotes.readlines())
+
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(password=password, user=my_gmail)
+        connection.sendmail(from_addr=my_gmail, to_addrs='maranataa10@gmail.com', msg=f'Subject:Quote\n\n{quote}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import smtplib
 #
 #
@@ -12,12 +49,12 @@
 
 
 
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-
-my_birthday = dt.datetime(year=2002, month=12,day=1)
-print(my_birthday)
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+#
+# my_birthday = dt.datetime(year=2002, month=12,day=1)
+# print(my_birthday)
