7a924cdd6fdfbda40b4ec143fb93df45f9092497
eyobScript <eyobbmulugeta@gmail.com>
2025-02-06 15:03:53 +0300

    birthday wisher 'day 32'

--- Day 32/birthday-wisher-extrahard-start/main.py
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -1,3 +1,4 @@
+import smtplib
 from datetime import datetime
 
 import pandas as pd
@@ -5,9 +6,10 @@ import random
 
 today  = datetime.now()
 today_tuple  = (today.month, today.day)
-
 data = pd.read_csv('birthdays.csv')
 
+my_gmail = 'eyobbmulugeta@gmail.com'
+password = 'ftgpfxwyebrazxdf'
 
 birthdays_dict = {(data_row['month'], data_row['day']): data_row for index, data_row in data.iterrows()}
 print()
@@ -17,5 +19,9 @@ if today_tuple in birthdays_dict:
 
     with open(file_path) as letter_file:
         content = letter_file.read()
-        content.replace('[NAME]', F"{birthdays_person["name"]}")
+        content = content.replace('[NAME]', birthdays_person["name"])
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_gmail, password=password)
+        connection.sendmail(from_addr=my_gmail ,to_addrs=birthdays_person['email'], msg=f"Subject:Happy Birthday\n\n{content}")
 
