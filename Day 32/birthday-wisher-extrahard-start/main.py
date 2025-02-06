bda88c8b04f945000f75aec72d8b2a783d4cd702
eyobScript <eyobbmulugeta@gmail.com>
2025-02-06 14:05:57 +0300

    birthday wisher 'day 32'

--- Day 32/birthday-wisher-extrahard-start/main.py
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -1,13 +1,21 @@
-##################### Extra Hard Starting Project ######################
+from datetime import datetime
 
-# 1. Update the birthdays.csv
+import pandas as pd
+import random
 
-# 2. Check if today matches a birthday in the birthdays.csv
+today  = datetime.now()
+today_tuple  = (today.month, today.day)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
+data = pd.read_csv('birthdays.csv')
 
 
+birthdays_dict = {(data_row['month'], data_row['day']): data_row for index, data_row in data.iterrows()}
+print()
+if today_tuple in birthdays_dict:
+    birthdays_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+
+    with open(file_path) as letter_file:
+        content = letter_file.read()
+        content.replace('[NAME]', F"{birthdays_person["name"]}")
 
