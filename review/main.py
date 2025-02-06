b460747d672e340a5be62401e79166632933409d
eyobScript <eyobbmulugeta@gmail.com>
2025-02-06 16:45:17 +0300

    'review csv'

---
+++ review/main.py
@@ -0,0 +1,13 @@
+# with open('weather_data.csv') as data:
+#     print(data.readlines())
+
+# import csv
+#
+# with open('weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
