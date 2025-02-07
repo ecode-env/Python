77e7c271af58aba957f11bf0fa6aa54074d88cee
eyobScript <eyobbmulugeta@gmail.com>
2025-02-07 14:47:42 +0300

    review csv

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,93 +1,33 @@
-# with open('weather-data.csv') as data:
-#     data_list = data.readlines()
-# print(data_list)
-import pandas
-# import csv
-# with open('weather-data.csv') as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != 'temp':
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-import pandas as pd
-
-#data = pd.read_csv('weather-data.csv')
-
-#print(data)
-
-## to change data to dictionary
-
-# data_dict = data.to_dict()
-#
-# print(data_dict)
-
-# to change the colum of data to list
-
-# data_list = data['temp'].to_list()
-#print(type(data_list)) ## series
-#print(data_list)
-
-## to calculate the average of colm temp
-
-#temp_av = sum(data_list) / len(data_list)
-#print(temp_av)
-
-## to calculate maximum of number with series method
-
-# data_max = data['temp'].max()
-# print(data_max)
-
-##to get data in columns
-
-#print(data.day)
-
-## to get data in row
-
-#print(data[data.day == 'Monday'])
-## to get the maximum value of temp row.
-#print(data[data.temp == data.temp.max()])
-
-## to get one item in the table
-
-# monday = data[data.day == 'Monday']
-# print(monday.condition)
-
-## to change from Celsius to Fahrenheit.
-
-# monday = data[data.day == 'Monday']
-# temp_celsius = monday.temp[0]
-# temp_fahrenheit = (temp_celsius * 9/5) + 32
-# print(temp_fahrenheit)
-
-
-## create data frame with pandas
-#
-# data_dict = {
-#     'student' : ['eyob', 'angela', 'jack'],
-#     'scores' : [70, 80, 60]
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
 # }
-# data_ = pandas.DataFrame(data_dict)
-# print(data_)
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
+import pandas
+#TODO 1. Create a dictionary in this format:
 
-##change top csv
+dataa = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-#print(data_.to_csv())
+nato_dict = {value.letter: value.code for index, value in dataa.iterrows()}
 
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-#-------------------------------------
-
-
-data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
-
-gray_squirrel = len(data[data['Primary Fur Color'] == 'Gray'])
-black_squirrel = len(data[data['Primary Fur Color'] == 'Black'])
-cinnamon_squirrel = len(data[data['Primary Fur Color'] == 'Cinnamon'])
-
-squirrel_fur_color = pd.DataFrame({
-    'gray': [gray_squirrel],
-    'red': [cinnamon_squirrel],
-    'black': [black_squirrel]
-
-})
-squirrel_fur_color.to_csv('new_fur_color.csv')
+user = input('Enter: ').upper()
+l = [nato_dict[letter] for letter in user]
+print(l)
