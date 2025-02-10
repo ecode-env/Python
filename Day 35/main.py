d879b25f109434d4f5b86fc17409312226453c47
eyobScript <eyobbmulugeta@gmail.com>
2025-02-10 12:10:07 +0300

    Day 35 'We have achieved first challenge'

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,1 +1,19 @@
-api_key = '007a9e0073b92ffca2a05e58bed8c638'
+import requests
+
+
+api_key = ''
+
+
+parameter = {
+    'lat': 9.033140,
+    'lon': 38.750080,
+    'appid': '007a9e0073b92ffca2a05e58bed8c638'
+}
+response = requests.get(url='https://api.openweathermap.org/data/2.5/forecast', params=parameter)
+
+data = response.json()
+
+print(data)
+
+
+
