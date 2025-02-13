3ff145ace749fa05940221dbfd34025f3f944143
eyobScript <eyobbmulugeta@gmail.com>
2025-02-13 19:41:01 +0300

    Day 46 project

---
+++ Day 46/main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+user_date = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD ')
+
+URL = f'https://www.billboard.com/charts/hot-100/{user_date}'
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+response = requests.get(url=URL, headers=header)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+top_music = [music.getText().strip() for music in soup.select(selector='li ul li h3')]
+print(len(top_music))
