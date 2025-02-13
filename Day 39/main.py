f1a0a07da429feaed5b1c08aa7f50245eb27e4b4
eyobScript <eyobbmulugeta@gmail.com>
2025-02-13 10:33:00 +0300

    Day 39 failed project cuz API

---
+++ Day 39/main.py
@@ -0,0 +1,18 @@
+import time
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
