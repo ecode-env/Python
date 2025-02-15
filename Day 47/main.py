096bcad0eb8e8b502f66eb95ce885255b684c2c5
eyobScript <eyobbmulugeta@gmail.com>
2025-02-15 17:34:36 +0300

    Day 46 project

--- Day 47/main.py
+++ Day 47/main.py
@@ -1,53 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import os
+from dotenv import load_dotenv
 
-PASSWORD = "ftgpfxwyebrazxdf"
-EMAIL = "eyobbmulugeta@gmail.com"
+# Load environment variables from the .env file
+load_dotenv()
 
+# Retrieve environment variables
+SMTP_ADDRESS = os.getenv("SMTP_ADDRESS")
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+# Check if environment variables are loaded
+if not all([SMTP_ADDRESS, EMAIL, PASSWORD]):
+    raise ValueError("Please check your .env file. Some environment variables are missing.")
+
+# URL of the product page
 URL = 'https://appbrewery.github.io/instant_pot/'
 
-headers= {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
+# Headers to mimic a real browser request
+headers = {
     "Accept-Language": "en-US,en;q=0.9",
-    "Host": "httpbin.org",
-    "Priority": "u=0, i",
-    "Sec-Ch-Ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\", \"Google Chrome\";v=\"132\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "\"Linux\"",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-    "X-Amzn-Trace-Id": "Root=1-67b09ebd-18596435288397dd65edf8c1"
-  }
+}
 
-
+# Send a GET request to the URL
 response = requests.get(url=URL, headers=headers)
 
-
+# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# Extract the price
 price = float(soup.find(name='span', class_='aok-offscreen').getText().split("$")[1])
 
-link = 'https://appbrewery.github.io/instant_pot/'
+# Extract the product title and remove extra spaces
 msg = str(soup.find(name='span', class_='product-title-word-break').getText()).replace(' ', '')
 
+# Print the product title
 print(msg)
 
+#Check if the price is below the threshold
 if price < 100:
     # Create the email message
     subject = "Amazon Price Alert!"
-    body = f"{msg}, 10 Programs now £{price}\n{link}"
+    body = f"{msg}, 10 Programs now £{price}\n{URL}"
 
     # Encode the message in UTF-8
     email_message = f"Subject: {subject}\n\n{body}".encode('utf-8')
 
     # Send the email
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    with smtplib.SMTP(SMTP_ADDRESS) as connection:
         connection.starttls()
         connection.login(user=EMAIL, password=PASSWORD)
         connection.sendmail(
