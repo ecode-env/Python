0ecb7f25e6128258fc4953c7cabcffaf904b1306
eyobScript <eyobbmulugeta@gmail.com>
2025-02-19 10:46:26 +0300

    Day 48 first challenge

--- Day 48/main.py
+++ Day 48/main.py
@@ -9,7 +9,7 @@ chrome_option.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome()
-driver.get(url='https://www.amazon.com')
+driver.get(url='https://www.python.org/')
 
 # price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
 # price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
@@ -17,21 +17,36 @@ driver.get(url='https://www.amazon.com')
 # print(f'The price is {price_dollar.text}.{price_cents.text}')
 
 
-search_bar = driver.find_element(By.NAME, value='q')
-print(search_bar.get_attribute('placeholder'))
+# search_bar = driver.find_element(By.NAME, value='q')
+# print(search_bar.get_attribute('placeholder'))
+#
+# button = driver.find_element(By.ID, value='submit')
+# print(button.size)
+#
+# documentation_link = driver.find_element(By.CLASS_NAME, value='.documentation-widget a')
+# print(documentation_link.text)
+#
+#
+# # XPATH
+#
+# bug_link = driver.find_element(By.XPATH, value='')
+# print(bug_link.text)
 
-button = driver.find_element(By.ID, value='submit')
-print(button.size)
+# Challenge 1
 
-documentation_link = driver.find_element(By.CLASS_NAME, value='.documentation-widget a')
-print(documentation_link.text)
+events = {}
+
+# Find the event times and names
+event_times = driver.find_elements(By.CSS_SELECTOR, value='.event-widget time')
+event_names = driver.find_elements(By.CSS_SELECTOR, value='.event-widget li a')
+
+for i in range(len(event_times)):
+    events[i] = {
+        'name': event_names[i].text,
+        'time': event_times[i].text,
+    }
 
 
-# XPATH
-
-bug_link = driver.find_element(By.XPATH, value='')
-print(bug_link.text)
-
-
+print(events)
 # driver.close() # close single
 driver.quit() # close entire tap
