3e068743ba9001654433fc5ae8a1720840323405
eyobScript <eyobbmulugeta@gmail.com>
2025-02-19 11:12:55 +0300

    Day 48 second challenge

---
+++ Day 48/intraction.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=chrome_option)
+driver.get(url='https://en.wikipedia.org/wiki/Main_Page')
+
+wiki = driver.find_elements(By.CSS_SELECTOR, value='#articlecount a')
+print(wiki[1].text)
+
+
+
+driver.close()
