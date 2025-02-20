dafe7044da6e6eab7bbb84658ae51028da647535
eyobScript <eyobbmulugeta@gmail.com>
2025-02-20 12:18:37 +0300

    Day 51 import package

--- Day 51/main.py
+++ Day 51/main.py
@@ -1,3 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+
+PROMISED_DOWN = 10
+PROMISED_UP = 5
+CHROME_DRIVER_PATH = ''
+TWITTER_EMAIL = 'codetestenv@gmail.com'
+TWITTER_PASSWORD = 234567890
