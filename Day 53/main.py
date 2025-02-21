b7142ba6af7e296ec66c012eaa13cf6f0e265874
eyobScript <eyobbmulugeta@gmail.com>
2025-02-21 16:07:47 +0300

    Day 53 STEP 2 we have found the list of house prices

--- Day 53/main.py
+++ Day 53/main.py
@@ -10,3 +10,8 @@ soup = BeautifulSoup(response, "html.parser")
 
 list_of_links = [ link.get('href') for link in soup.find_all(name='a', class_='property-card-link') ]
 
+#STEP 2
+
+list_of_prices = [price.text.split('+')[0].split('/')[0]
+                  for price in soup.find_all(name='span', class_='PropertyCardWrapper__StyledPriceLine')]
+
