09c1c89dcd084353281ffdaaf7714fc2e437742d
eyobScript <eyobbmulugeta@gmail.com>
2025-02-21 13:47:35 +0300

    Day 52 whe have finished our project

--- Day 52/main.py
+++ Day 52/main.py
@@ -1,5 +1,12 @@
+from InstaFollower import InstaFollower
 
+PASSWORD = ''
+EMAIL = 'eyobbmulugeta@gmail.com'
 
+bot = InstaFollower()
+bot.login(EMAIL, PASSWORD)
+bot.find_followers()
+bot.follow()
 
 
 
