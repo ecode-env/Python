89553ee5d9b8cdf38df7a0749193287f7a965b19
eyobScript <eyobbmulugeta@gmail.com>
2025-02-21 16:19:04 +0300

    Day 53 we have created class for BeautifulSoup function and we commented the first logic

--- Day 53/main.py
+++ Day 53/main.py
@@ -2,21 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
-response = requests.get(url='https://appbrewery.github.io/Zillow-Clone/').text
-
-soup = BeautifulSoup(response, "html.parser")
-
-#STEP 1
-
-list_of_links = [ link.get('href') for link in soup.find_all(name='a', class_='property-card-link') ]
-
-#STEP 2
-
-list_of_prices = [price.text.split('+')[0].split('/')[0]
-                  for price in soup.find_all(name='span', class_='PropertyCardWrapper__StyledPriceLine')]
-
-#STEP 3
-
-
-list_of_addresses = [ address.text.strip().replace('|', '').strip() for address  in soup.find_all(name='address') ]
-
+# response = requests.get(url='https://appbrewery.github.io/Zillow-Clone/').text
+#
+# soup = BeautifulSoup(response, "html.parser")
+#
+# #STEP 1
+#
+# list_of_links = [ link.get('href') for link in soup.find_all(name='a', class_='property-card-link') ]
+#
+# #STEP 2
+#
+# list_of_prices = [price.text.split('+')[0].split('/')[0]
+#                   for price in soup.find_all(name='span', class_='PropertyCardWrapper__StyledPriceLine')]
+#
+# #STEP 3
+#
+#
+# list_of_addresses = [ address.text.strip().replace('|', '').strip() for address  in soup.find_all(name='address') ]
+#
