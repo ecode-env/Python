d3fd117d40be97c7558ac73264730af21b7e118f
eyobScript <eyobbmulugeta@gmail.com>
2025-02-24 16:42:13 +0300

    Day 55

--- Day 55/main.py
+++ Day 55/main.py
@@ -3,20 +3,34 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_underline(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+def make_emphasize(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+
 @app.route('/')
 def hello():
     return 'hello'
 
-
 @app.route('/bye')
+@make_underline
+@make_bold
+@make_emphasize
 def bye():
     return 'bye!'
 
-
 @app.route('/username/<name>/<int:number>')
 def username(name, number):
-    return f"hello {name}! you are {number} years old"
-
+    return f"Hello {name}! You are {number} years old."
 
 if __name__ == '__main__':
     app.run(debug=True)
