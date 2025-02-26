512165f43b9b85d8458b80a409fb98ce62ae4cd0
eyobScript <eyobbmulugeta@gmail.com>
2025-02-26 11:28:43 +0300

    Day 57 task 3

--- Day 57/server.py
+++ Day 57/server.py
@@ -19,8 +19,17 @@ def guess(name):
     return render_template(template_name_or_list="guess.html",
                            name=name,
                            age=age,
-                           gender=gender
-                           )
+                           gender=gender)
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    all_posts = requests.get(url=blog_url).json()
+    return render_template(template_name_or_list='blog.html', posts=all_posts)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
