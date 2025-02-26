d28fd2edc6d86d7cd79042aeeb7f88f827b1f7d7
eyobScript <eyobbmulugeta@gmail.com>
2025-02-26 10:31:29 +0300

    Day 57 task 2

--- Day 57/server.py
+++ Day 57/server.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    current_year = year = datetime.now().year
+    current_year = datetime.now().year
     random_number = random.randint(1,9)
     return render_template(template_name_or_list="index.html",num=random_number,  CURRENT_YEAR=current_year )
 
 
 @app.route('/guess/<name>')
 def guess(name):
+    gender = requests.get(url=f"https://api.genderize.io/?name={name}").json()['gender']
+    age = requests.get(url=f"https://api.agify.io?name={name}").json()["age"]
 
-    return render_template(template_name_or_list="index.html")
 
+    return render_template(template_name_or_list="guess.html",
+                           name=name,
+                           age=age,
+                           gender=gender
+                           )
 
 if __name__ == "__main__":
     app.run(debug=True)
