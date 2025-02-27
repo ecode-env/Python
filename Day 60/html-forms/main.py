d9958a3c001186b4d26f5c4a61e1f3523868d473
eyobScript <eyobbmulugeta@gmail.com>
2025-02-27 14:08:29 +0300

    Day 60 we have done project

--- Day 60/html-forms/main.py
+++ Day 60/html-forms/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+
 
 
 posts = requests.get("https://api.npoint.io/d9d504779f838de6e2b3").json()
@@ -17,8 +18,16 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+# SOLUTION to Challenge:
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html", successfull="Successfully sent your message")
     return render_template("contact.html")
 
 
@@ -30,11 +39,8 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
-@app.route("/form-entry")
-def receive_data():
-    return "Successfully sent your message"
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
