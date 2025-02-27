5f45e7fe43ccbda67ad2caa5825a91f53ab36aeb
eyobScript <eyobbmulugeta@gmail.com>
2025-02-27 11:07:44 +0300

    Day 59 we have done blog project

--- Day 59/upgraded blog/main.py
+++ Day 59/upgraded blog/main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+blog_url = "https://api.npoint.io/d9d504779f838de6e2b3"
+all_posts = requests.get(url=blog_url).json()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template(template_name_or_list="index.html", posts=all_posts)
 
 
 @app.route('/about')
@@ -15,6 +18,11 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    selected_blog = [post for post in all_posts if post['id'] == int(post_id)][0]
+    return render_template('post.html', posts=selected_blog)
 
 if __name__ == "__main__":
     app.run(debug=True)
