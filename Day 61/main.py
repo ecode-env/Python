ede224e1d0fe0661bc59bfee5c3143127ae3340a
eyobScript <eyobbmulugeta@gmail.com>
2025-02-27 17:23:39 +0300

    Day 61 task 2

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField,SubmitField
+from wtforms.validators import DataRequired, ValidationError
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
 
 
 app = Flask(__name__)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+app.secret_key = "my_name_is_eyob_mulugeta"
 
 
 @app.route("/")
@@ -17,9 +20,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
