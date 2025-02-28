9a1a259105d97e2f72b73ada34045bc8b539c94b
eyobScript <eyobbmulugeta@gmail.com>
2025-02-28 13:28:02 +0300

    Day 61

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import PasswordField
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
+class MyForm(FlaskForm):
+    email = StringField(label='email', validators=[DataRequired()])
+    password = PasswordField(label='password', validators=[DataRequired()])
 
 
 app = Flask(__name__)
+app.secret_key = 'b_5#y2L"F4Q8z:{}]/'
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    form = MyForm()
+    return render_template("login.html", form=form)
+
 
 
 if __name__ == '__main__':
