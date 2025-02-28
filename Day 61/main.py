3a5a091dab60e00115c0d107a2ab7def1b1c4bb9
eyobScript <eyobbmulugeta@gmail.com>
2025-02-28 14:22:32 +0300

    Day 61

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import PasswordField,StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
+
 
 class MyForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(message="Invalid email format. Must contain '@' and '.'")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
+
 
 
 app = Flask(__name__)
@@ -17,9 +19,10 @@ app.secret_key = 'b_5#y2L"F4Q8z:{}]/'
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route(rule="/login", methods=['POST','GET'])
 def login():
     form = MyForm()
+    form.validate_on_submit()
     return render_template("login.html", form=form)
 
 
