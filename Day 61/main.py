867788733a00ab5c78a3b45b2e4fe23ecce9968b
eyobScript <eyobbmulugeta@gmail.com>
2025-02-28 13:14:28 +0300

    Day 61 task 2

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,30 +1,25 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,SubmitField
-from wtforms.validators import DataRequired, ValidationError
 
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
 
-class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label='Log In')
+On Windows type:
+python -m pip install -r requirements.txt
 
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
 
 
 app = Flask(__name__)
-app.secret_key = "my_name_is_eyob_mulugeta"
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    login_form.validate_on_submit()
-    return render_template('login.html', form=login_form)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
