880eea125ce0381409f5606631f6a074ed8b3e07
eyobScript <eyobbmulugeta@gmail.com>
2025-03-01 11:55:06 +0300

    Day 63 project

--- Day 63/main.py
+++ Day 63/main.py
@@ -9,11 +9,18 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template(template_name_or_list='index.html', books=all_books)
 
 
-@app.route("/add", methods=['POST'])
+@app.route("/add", methods=['POST','GET'])
 def add():
+    if request.method == 'POST':
+        all_books.append({
+            'title': request.form['name'],
+            'author': request.form['author'],
+            'rating': request.form['rate']
+        })
+        return redirect(url_for('home'))
 
     return render_template('add.html')
 
