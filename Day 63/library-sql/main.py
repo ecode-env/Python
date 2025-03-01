101657d12a34b3ea5b51ae2cd37ebf6c7154b5c3
eyobScript <eyobbmulugeta@gmail.com>
2025-03-01 15:46:26 +0300

    Day 63 project

--- Day 63/library-sql/main.py
+++ Day 63/library-sql/main.py
@@ -6,13 +6,15 @@ from sqlalchemy import Integer, String, Float
 
 
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE books ("
-#                "id INTEGER PRIMARY KEY,"
-#                "title varchar(250) NOT NULL UNIQUE,"
-#                "author varchar(250) NOT NULL, "
-#                "rating FLOAT NOT NULL)")
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# # cursor.execute("CREATE TABLE books ("
+# #                "id INTEGER PRIMARY KEY,"
+# #                "title varchar(250) NOT NULL UNIQUE,"
+# #                "author varchar(250) NOT NULL, "
+# #                "rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+
