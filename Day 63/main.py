0f7b66277c690652e4f1bec8d6c8e9f289fb068d
eyobScript <eyobbmulugeta@gmail.com>
2025-03-01 16:37:03 +0300

    Day 63 project

--- Day 63/main.py
+++ Day 63/main.py
@@ -1,30 +1,62 @@
-from flask import Flask, render_template, request, redirect, url_for
+# from flask import Flask, render_template, request, redirect, url_for
+#
+#
+#
+# app = Flask(__name__)
+#
+# all_books = []
+#
+#
+# @app.route('/')
+# def home():
+#     return render_template(template_name_or_list='index.html', books=all_books)
+#
+#
+# @app.route("/add", methods=['POST','GET'])
+# def add():
+#     if request.method == 'POST':
+#         all_books.append({
+#             'title': request.form['name'],
+#             'author': request.form['author'],
+#             'rating': request.form['rate']
+#         })
+#         return redirect(url_for('home'))
+#
+#     return render_template('add.html')
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+#
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
 
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-all_books = []
-
-
-@app.route('/')
-def home():
-    return render_template(template_name_or_list='index.html', books=all_books)
+db = SQLAlchemy(app)
 
 
-@app.route("/add", methods=['POST','GET'])
-def add():
-    if request.method == 'POST':
-        all_books.append({
-            'title': request.form['name'],
-            'author': request.form['author'],
-            'rating': request.form['rate']
-        })
-        return redirect(url_for('home'))
+class Book(db.Model):
+    __tablename__ = 'books'
 
-    return render_template('add.html')
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    balance: Mapped[float] = mapped_column(Float, default=0.0)
+
+    def __repr__(self):
+        return f'<Book {self.name}>'
 
 
-if __name__ == "__main__":
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
     app.run(debug=True)
-
