efe33a877aab3152c865b16677e85299953d6a2f
eyobScript <eyobbmulugeta@gmail.com>
2025-03-03 14:15:17 +0300

    Day 62 project "import all requirement that we will use for"

--- Day 63/sqlite/main.py
+++ Day 63/sqlite/main.py
@@ -1,15 +1,21 @@
-import sqlite3
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db")
+#
+# cursor = db.cursor()
+# # cursor.execute("CREATE TABLE books ("
+# #                "id INTEGER PRIMARY KEY, "
+# #                "title varchar(250) NOT NULL UNIQUE, "
+# #                "author varchar(250) NOT NULL, "
+# #                "rating FLOAT NOT NULL)")
+#
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+# db.close()
 
-db = sqlite3.connect("books-collection.db")
 
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE books ("
-#                "id INTEGER PRIMARY KEY, "
-#                "title varchar(250) NOT NULL UNIQUE, "
-#                "author varchar(250) NOT NULL, "
-#                "rating FLOAT NOT NULL)")
-
-
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
-db.close()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
