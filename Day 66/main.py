83d2ed99620fc401dad84d7b692a5e5b546ca0d2
eyobScript <eyobbmulugeta@gmail.com>
2025-03-05 14:18:54 +0300

    Day 66 create GET method to get random cafe

--- Day 66/main.py
+++ Day 66/main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -28,6 +30,20 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    # def to_dict(self):
+    #     # Method 1.
+    #     dictionary = {}
+    #     # Loop through each column in the data record
+    #     for column in self.__table__.columns:
+    #         # Create a new dictionary entry;
+    #         # where the key is the name of the column
+    #         # and the value is the value of the column
+    #         dictionary[column.name] = getattr(self, column.name)
+    #     return dictionary
+    #
+    #     # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+    #     return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -42,7 +58,42 @@ def home():
 
 @app.route("/random")
 def get_random_cafe():
-  pass
+
+    random_cafe = random.choice(Cafe.query.all())
+    #return jsonify(cafe=random_cafe.to_dict())
+    return jsonify(
+        cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
+    # return jsonify(cafe={
+    #     # Omit the id from the response
+    #     # "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #
+    #     # Put some properties in a sub-category
+    #     "amenities": {
+    #         "seats": random_cafe.seats,
+    #         "has_toilet": random_cafe.has_toilet,
+    #         "has_wifi": random_cafe.has_wifi,
+    #         "has_sockets": random_cafe.has_sockets,
+    #         "can_take_calls": random_cafe.can_take_calls,
+    #         "coffee_price": random_cafe.coffee_price,
+    #     }
+    # })
+
 
 # HTTP POST - Create Record
 
