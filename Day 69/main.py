7c4a6a924a9d27df361e7c6c5cededf0f3e325f8
eyobScript <eyobbmulugeta@gmail.com>
2025-03-07 20:04:50 +0300

    Day 69 gravatar

--- Day 69/main.py
+++ Day 69/main.py
@@ -197,7 +197,7 @@ def show_post(post_id):
 
     comment_form = CommentForm()
 
-    if comment_form.validate_on_submit():
+    if request.method == 'POST':
         if not current_user.is_authenticated:
             flash('You need to login or register to comment.')
             return redirect(url_for('login'))
