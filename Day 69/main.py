2fc7312991746623fb39cf0b78481465c4f4bc78
eyobScript <eyobbmulugeta@gmail.com>
2025-03-07 17:39:41 +0300

    Day 69 create relationship with user and blog

--- Day 69/main.py
+++ Day 69/main.py
@@ -188,7 +188,7 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=str(current_user.id),
+            author=current_user,
             date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
