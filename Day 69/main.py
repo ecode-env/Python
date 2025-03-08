c49f6d0c70de35dacee820d7cdaa97adfc8855c7
eyobScript <eyobbmulugeta@gmail.com>
2025-03-08 09:14:42 +0300

    Day 69 gravatar

--- Day 69/main.py
+++ Day 69/main.py
@@ -57,13 +57,10 @@ class BlogPost(db.Model):
     __tablename__ = "blog_posts"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
     # Create Foreign Key, "users.id" the users refers to the tablename of User.
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id", ondelete="CASCADE"))
-
     # Create reference to the User object. The "posts" refers to the posts property in the User class.
     author = relationship(argument="User", back_populates="posts")
-
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
     date: Mapped[str] = mapped_column(String(250), nullable=False)
